9c8449bb1bb5244f517b624ec28486123cbe437c
SyedMinhaj1 <156704395+SyedMinhaj1@users.noreply.github.com>
2024-07-31 23:20:46 +0500

    EDITED

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Landingpage.css";
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+      <div className='textstructure '>
+        <div className='masker'>
+          <div className='text text1'>
+            <h1>We Create</h1>
+          </div>
+          <div className='text text2'>
+            <h1>Eye Opening</h1>
+          </div>
+          <div className='text text3'>
+            <h1>Presentation</h1>
+          </div> 
+        </div>
+      </div>
+      <div className='bottom'>
+        <p>For public and private companies</p>
+        <p>
+        From the first pitch to IPO</p>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
