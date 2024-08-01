f2f0633407fd8180e29babbe77484e9b097992ad
SyedMinhaj1 <156704395+SyedMinhaj1@users.noreply.github.com>
2024-08-02 00:04:54 +0500

    ERROR FIXED

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import LandingPage from './components/LandingPage/LandingPage'
+import Marquee from './components/Marquee/Marquee'
 
 function App() {
   return (
     <div className='w-full h-screen text-white'>
       <Navbar />
       <LandingPage />
+      <Marquee />
     </div>
   )
 }
--- src/components/marquee/marquee.jsx
+++ src/components/marquee/marquee.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import "./marquee.css";
+import "./Marquee.css";
 
 const marquee = () => {
   return (
     <div className='w-full h-[50vh] bg-green-900 '>
-
+      <h1>this a arquee</h1>
     </div>
   )
 }
