8edd1d85a27bdd8058f29b192748b175170bc1e6
Bilal Jadi <bilalcodes777@gmail.com>
2024-08-02 14:07:04 -0700

    and jsx in name

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
