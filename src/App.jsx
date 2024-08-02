9cdb1fe7e6050e9c6db8646b366ecf414d5d4738
SyedMinhaj1 <156704395+SyedMinhaj1@users.noreply.github.com>
2024-08-03 02:16:23 +0500

    updated about session

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import LandingPage from './components/LandingPage/LandingPage'
 import Marquee from './components/marquee/marquee'
+import About from './components/About/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <LandingPage />
       <Marquee />
+      <About />
     </div>
   )
 }
