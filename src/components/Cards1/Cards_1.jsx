5fd786b28e869a13308366725d4924a1f320cb6b
Bilal Jadi <bilalcodes777@gmail.com>
2024-08-02 14:14:35 -0700

    card_1 heading

---
+++ src/components/Cards1/Cards_1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Cards_1() {
+  return (
+    <div className='w-full py-10'>
+        <div className='w-full px-10'>
+            <h1>Card-1</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Cards_1
