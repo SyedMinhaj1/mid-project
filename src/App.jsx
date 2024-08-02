70fab2364807bd66e48dc2bc453105b89af3fac9
Bilal Jadi <bilalcodes777@gmail.com>
2024-08-02 12:17:24 -0700

    what

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import LandingPage from './components/LandingPage/LandingPage'
-import Marquee from './components/marquee/marquee'
+import Marquee from './components/Marquee/Marquee'
 
 function App() {
   return (
